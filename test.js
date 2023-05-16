b194806f6195fcbcfd20ea21c0765e03552f43e2
clovis eyong nkem <cloviseyong@yahoo.com>
2023-05-16 11:33:12 +0700

    init

---
+++ test.js
@@ -0,0 +1,20 @@
+// function changeColor() {
+//   document.getElementById("demo").style.color="red";
+//   document.getElementById('demo').innerHTML=5+5;
+//   document.write(11+3);
+// }
+
+// document.write(11+3);
+
+// function myFunction(){
+//   document.getElementById("demo1").innerHTML="Hello Dolly";
+//   document.getElementById("demo2").innerHTML="How are you";
+// }
+
+// let x;
+// x = 6;
+// document.getElementById("demo").innerHTML = x;
+
+function myFunction(){
+  document.getElementById("demo1").innerHTML="Same here";
+}
