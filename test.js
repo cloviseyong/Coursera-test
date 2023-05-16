11576acf44abea1f350931649c95f7d3367f5b24
clovis eyong nkem <cloviseyong@yahoo.com>
2023-05-16 12:04:59 +0700

    init

--- test.js
+++ test.js
@@ -16,5 +16,19 @@
 // document.getElementById("demo").innerHTML = x;
 
 function myFunction(){
-  document.getElementById("demo1").innerHTML="Same here";
+  document.getElementById("demo").style.color="red";
+}
+
+function myFunction1(){
+  document.getElementById("demo1").innerHTML="hello thanks for coming";
+}
+
+
+function myFunction2(){
+  document.getElementById("demo3").style.fontSize ="35px";
+}
+
+function myFunction3(){
+  document.getElementById("demo4").innerHTML="I love you Kim";
+
 }
